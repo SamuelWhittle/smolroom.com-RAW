ab3c1cccbff1e406dad87b656fef325c26a1ee11
Samuel Whittle <swbnp7@gmail.com>
2020-06-19 22:14:23 -0500

    n-dimensional constructor added, basic html and css finished, next I have to start the hard part :/

--- perlinNoise/src/main.js
+++ perlinNoise/src/main.js
@@ -1,4 +1,4 @@
-//----------- functions
+//-----------Functions-----------
 // Sets canvas size to be relative to the chrome window
 function adjustCanvasSize() {
     c.width = c.offsetWidth;
@@ -10,24 +10,20 @@ function draw() {
     // Make sure the canvas is the right size in pixels to match the parent div in the html
     adjustCanvasSize();
 
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(10, 10, 50, 50);
-}
-
-function redrawTime() {
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, c.width, c.height);
 
-    ctx.fillStyle = "red";
-    ctx.fillRect(timeline.value, 10, 50, 50);
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(timelineInput.value/100*(c.width-50), 10, 50, 50);
 }
 
-// Where everything including calculations happens
-function main() {
+// Redraws canvas based on selected time
+function redrawTime() {
+    //idk change somehow dependant on timeline? I'll figure it out later.
     draw();
 }
 
-//---------- HTML Elements
+//----------HTML Elements-----------
 // Get Canvas
 var c = document.getElementById("mainCanvas");
 var ctx = c.getContext("2d");
@@ -35,12 +31,16 @@ var ctx = c.getContext("2d");
 // Get Input Slider
 var timelineInput = document.getElementById("timeline");
 
-//---------- Event Listeners
+//----------Event Listeners-----------
 // Window Resize
 window.addEventListener('resize', draw);
 
 // Timeline move
 timelineInput.addEventListener('input', redrawTime);
 
-//---------- Do The Things
-main();
+//----------Perlin Noise Object-----------
+// perlinNoise([dim1, dim2, dim3, ..., dimN, gridMeter, numOctaves, octaveScale])
+var noise = new perlinNoise([c.width, c.height, 100, 30, 4, 1/2]);
+
+//----------Do The Things-----------
+draw();
--- perlinNoise/src/perlinNoise.js
+++ perlinNoise/src/perlinNoise.js
@@ -1,1 +1,11 @@
 console.log("perlinNoise.js");
+class perlinNoise {
+    constructor(args) {
+        this.dimensions = args;
+
+        this.octaveScale = this.dimensions.splice(this.dimensions.length - 1, 1)[0];
+        this.numOctaves = this.dimensions.splice(this.dimensions.length - 1, 1)[0];
+        this.gridMeter = this.dimensions.splice(this.dimensions.length - 1, 1)[0];
+        console.log(this.dimensions, this.octaveScale, this.numOctaves, this.gridMeter);
+    }
+}
